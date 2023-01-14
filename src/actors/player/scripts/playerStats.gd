ef0a2400215c5743a66a4732cbe0ad5d0a3a0a02
Matt <mjcutshaw@gmail.com>
2023-01-14 00:22:15 -0500

    jump higher

--- src/actors/player/scripts/playerStats.gd
+++ src/actors/player/scripts/playerStats.gd
@@ -7,4 +7,4 @@ var health: int = 3
 var baseSpeed: int = 25
 var turboSpeed #min speed for wall run  #TODO: make based off base speed
 
-var baseJumpHeight: float = 4.5
+var baseJumpHeight: float = 6.25
