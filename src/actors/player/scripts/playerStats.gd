0fc611e830a05584df286621ad2f33b8efe46f57
Matt <mjcutshaw@gmail.com>
2023-01-20 05:01:39 -0500

    slower accel/friction in air

--- src/actors/player/scripts/playerStats.gd
+++ src/actors/player/scripts/playerStats.gd
@@ -8,7 +8,7 @@ var baseSpeed: int = 25
 var turboSpeed #min speed for wall run  #TODO: make based off base speed
 var baseAcceleration: float = 1.2
 var baseFriction: float = 1.0
-var airModifier: float = 0.8
+var airModifier: float = 1.2
 
 var baseJumpHeight: float = 6.25
 var terminalVelocityModifier: float = 2
