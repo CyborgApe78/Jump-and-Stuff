f0a1491e097afdc6868d08db16fee8651954868c
Matt <mjcutshaw@gmail.com>
2023-01-24 03:35:59 -0500

    add horizontal check to fall

--- src/actors/player/states/fall.gd
+++ src/actors/player/states/fall.gd
@@ -28,6 +28,7 @@ func exit() -> void:
 
 
 func physics(delta) -> void:
+	player.attempt_horizontal_corner_correction(jumpCornerCorrectionHorizontal, delta)
 	player.attempt_vertical_corner_correction(jumpCornerCorrectionVertical, delta)
 	
 	player.move_and_slide()
