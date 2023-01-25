1b0f81376236869bd0b259a248a50f30007cb2e0
Matt <mjcutshaw@gmail.com>
2023-01-25 05:56:10 -0500

    lowered jump height

--- src/actors/player/scripts/playerStats.gd
+++ src/actors/player/scripts/playerStats.gd
@@ -10,5 +10,5 @@ var baseAcceleration: float = 1.2
 var baseFriction: float = 1.0
 var airModifier: float = 1.2
 
-var baseJumpHeight: float = 6.25
+var baseJumpHeight: float = 4.25
 var terminalVelocityModifier: float = 2
