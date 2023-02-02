efeb4e02c3b42f8d860feee13f2cde939b659dac
Matt <mjcutshaw@gmail.com>
2023-02-01 22:30:39 -0500

    die state started

---
+++ src/actors/player/states/die.gd
@@ -0,0 +1,38 @@
+extends PlayerInfo
+
+
+
+func enter() -> void:
+	pass
+#	GameStats.die += 1
+
+
+func exit() -> void:
+	pass
+#	player.previousVelocity = player.velocity
+
+
+func physics(_delta) -> void:
+	pass	
+
+
+func visual(_delta) -> void:
+	pass
+
+
+func sound(_delta: float) -> void:
+	pass
+
+
+func handle_input(_event: InputEvent) -> int:
+	
+
+	return State.Null
+
+
+func state_check(_delta: float) -> int:
+	
+
+	
+
+	return State.Null
