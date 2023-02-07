464c492fa3270d3cd2e0b522cf24d4c21f7495e9
Matt <mjcutshaw@gmail.com>
2023-02-07 17:29:54 -0500

    various wall jumps

--- src/actors/player/states/jumpWall.gd
+++ src/actors/player/states/jumpWall.gd
@@ -1,36 +1,49 @@
 extends PlayerInfo
+#TODO: create place to test jumps
 
+@export var timerLock: Timer
 
 var wallHop: bool
+var jumpDirection: int #TODO: change to jumpDirection 
 
 
 func enter() -> void:
-	player.wall_detection()
+	timerLock.start()
+	jumpDirection = player.wall_detection(30)
 	EventBus.actionAnnounce.emit("Boing")
 	topSpeed = 0
 	neutral_move_direction_logic()
 	player.sounds.jump.play()
-	player.timers.coyoteJumpWall.stop()
-#	if player.moveDirection.y == -1:
-		#TODO: make up jump different
-#	elif player.moveDirection.x == 0:
-		#TODO: make no directiom jump different
-#	if player.moveDirection.x == player.lastWallDirection:
-		#TODO: make same directiom jump different
-	if player.moveDirection.x == player.wallDirection:
-		player.velocity = Vector2(50 * player.wallDirection, jumpVelocity)
+	
+	if !player.timers.coyoteJumpWall.is_stopped():
+		jumpDirection = player.lastWallDirection
+		player.timers.coyoteJumpWall.stop()
+	
+	if player.moveDirection.y == -1:
+		player.velocity = Vector2(100 * -jumpDirection, jumpVelocity * 1.2)
+		EventBus.playerInfo.emit("Wall Up")
+	elif player.moveDirection.y == 1:
+		player.velocity = Vector2(100 * -jumpDirection, 100)
+		EventBus.playerInfo.emit("Wall Down")
+	elif player.moveDirection.x == 0:
+		EventBus.playerInfo.emit("Wall Neutral")
+		player.velocity = Vector2(moveSpeed * -jumpDirection, jumpVelocity * 1.2)
+	elif player.moveDirection.x == -jumpDirection:
+		EventBus.playerInfo.emit("Wall Away")
+		player.velocity = Vector2(moveSpeed * -jumpDirection, jumpVelocity)
+	elif player.moveDirection.x == jumpDirection:
+		EventBus.playerInfo.emit("Wall Towards")
+		player.velocity = Vector2(50 * jumpDirection, jumpVelocity * 0.8)
 		wallHop = true
-	else:
-		player.velocity.y = jumpVelocity
-		player.velocity.x = (moveSpeed/2) * -player.wallDirection
-	player.particles.jumpWall.restart()
+	
+	player.particles.jumpWall.restart() #TODO: get direction from wall direction
+
 
 func exit() -> void:
 	pass
 
 
 func physics(delta) -> void:
-	
 	player.attempt_horizontal_corner_correction(jumpCornerCorrectionHorizontal, delta)
 	player.attempt_vertical_corner_correction(jumpCornerCorrectionVertical, delta)
 	
@@ -38,11 +51,8 @@ func physics(delta) -> void:
 	
 	gravity_logic(gravityJump, delta)
 	
-	if !wallHop:
-		if player.moveDirection.x == 0:
-			momentum_logic(moveSpeed, false)
-		else:
-			momentum_logic(moveSpeed, true)
+	#TODO: air velocity func
+#	air_velocity_logic(moveSpeed, accelerationAir, frictionAir, delta)
 	
 	track_top_speed(player.velocity.x)
 
@@ -56,45 +66,47 @@ func sound(delta: float) -> void:
 
 
 func handle_input(event: InputEvent) -> int:
-	if Input.is_action_just_released("jump"):
-		player.velocity.y = max( player.velocity.y, jumpVelocity * percentMinJumpVelocity)
-		return State.Fall
-	if Input.is_action_just_pressed("glide")  and abilities.can_use(PlayerAbilities.list.Glide):
-		return State.Glide
-	if Input.is_action_just_pressed("dive")  and abilities.can_use(PlayerAbilities.list.Dive):
-		return State.Dive
-	if Input.is_action_just_pressed("ground pound") and abilities.can_use(PlayerAbilities.list.GroundPound):
-			return State.GroundPound
-	if Input.is_action_just_pressed("dash"):
-		dash_pressed_buffer()
+	if timerLock.is_stopped():
+		if Input.is_action_just_released("jump"):
+			player.velocity.y = max( player.velocity.y, jumpVelocity * percentMinJumpVelocity)
+			return State.Fall
+		if Input.is_action_just_pressed("glide")  and abilities.can_use(PlayerAbilities.list.Glide):
+			return State.Glide
+		if Input.is_action_just_pressed("dive")  and abilities.can_use(PlayerAbilities.list.Dive):
+			return State.Dive
+		if Input.is_action_just_pressed("ground pound") and abilities.can_use(PlayerAbilities.list.GroundPound):
+				return State.GroundPound
+		if Input.is_action_just_pressed("dash"):
+			dash_pressed_buffer()
 
 	return State.Null
 
 
 func state_check(delta: float) -> int:
-	if player.is_on_ceiling():
-		consecutive_jump_cancel()
-		return State.Fall
-	if player.is_on_wall() and topSpeed > moveSpeed: #TODO: wallland if going up
-		topSpeed = 0
-		return State.BonkAir
-	if player.velocity.y > -jumpApexHeight:
-		return State.JumpApex
-	if player.is_on_floor():
-		player.landed()
-		if player.velocity.x != 0:
-			return State.Walk
-		else:
-			return State.Idle
-	if dashBufferState != State.Null:
-		if abilities.can_use(PlayerAbilities.list.DashSide) and dashBufferState == State.DashAir:
-			dashBufferState = State.Null
-			return State.DashAir
-		if abilities.can_use(PlayerAbilities.list.DashUp) and dashBufferState == State.DashUp:
-			dashBufferState = State.Null
-			return State.DashUp
-		if abilities.can_use(PlayerAbilities.list.DashDown) and dashBufferState == State.DashDown:
-			dashBufferState = State.Null
-			return State.DashDown
+	if timerLock.is_stopped():
+		if player.is_on_ceiling():
+			consecutive_jump_cancel()
+			return State.Fall
+		if player.is_on_wall() and topSpeed > moveSpeed: #TODO: wallland if going up
+			topSpeed = 0
+			return State.BonkAir
+		if player.velocity.y > -jumpApexHeight:
+			return State.JumpApex
+		if player.is_on_floor():
+			player.landed()
+			if player.velocity.x != 0:
+				return State.Walk
+			else:
+				return State.Idle
+		if dashBufferState != State.Null:
+			if abilities.can_use(PlayerAbilities.list.DashSide) and dashBufferState == State.DashAir:
+				dashBufferState = State.Null
+				return State.DashAir
+			if abilities.can_use(PlayerAbilities.list.DashUp) and dashBufferState == State.DashUp:
+				dashBufferState = State.Null
+				return State.DashUp
+			if abilities.can_use(PlayerAbilities.list.DashDown) and dashBufferState == State.DashDown:
+				dashBufferState = State.Null
+				return State.DashDown
 
 	return State.Null
