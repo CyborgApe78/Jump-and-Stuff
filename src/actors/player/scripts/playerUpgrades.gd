71764b5ead48a99af69f49dae98589bd9d5b1422
Matt <mjcutshaw@gmail.com>
2023-02-10 03:12:53 -0500

    created upgrade unlock resource

---
+++ src/actors/player/scripts/playerUpgrades.gd
@@ -0,0 +1,73 @@
+extends Resource
+class_name PlayerUpgrades
+
+
+var unlockedProtectionHeat: bool = false:
+	set(v):
+		unlockedProtectionHeat = v
+		if unlockedProtectionHeat:
+			print("heat ulock")
+			EventBus.playerUpgradeUnlock.emit(list.protectionHeat)
+
+var unlockedProtectionCold: bool = false:
+	set(v):
+		unlockedProtectionCold = v
+		if unlockedProtectionCold:
+			EventBus.playerUpgradeUnlock.emit(list.protectionCold)
+
+var unlockedProtectionWater: bool = false:
+	set(v):
+		unlockedProtectionWater = v
+		if unlockedProtectionWater:
+			EventBus.playerUpgradeUnlock.emit(list.protectionWater)
+
+var unlockedProtectionAcid: bool = false:
+	set(v):
+		unlockedProtectionAcid = v
+		if unlockedProtectionAcid:
+			EventBus.playerUpgradeUnlock.emit(list.protectionAcid)
+
+var unlockedProtectionLava: bool = false:
+	set(v):
+		unlockedProtectionLava = v
+		if unlockedProtectionLava:
+			EventBus.playerUpgradeUnlock.emit(list.protectionLava)
+
+
+enum list {
+	Null,
+	All,
+	protectionAll,
+	protectionHeat,
+	protectionCold,
+	protectionWater,
+	protectionAcid,
+	protectionLava,
+	}
+
+
+func unlock(upgrade: int, BOOL:bool) -> void:
+	if upgrade == list.All:
+		unlockedProtectionHeat = BOOL
+		unlockedProtectionCold = BOOL
+		unlockedProtectionWater = BOOL
+		unlockedProtectionAcid = BOOL
+		unlockedProtectionLava = BOOL
+	elif upgrade == list.protectionAll:
+		unlockedProtectionHeat = BOOL
+		unlockedProtectionCold = BOOL
+		unlockedProtectionWater = BOOL
+		unlockedProtectionAcid = BOOL
+		unlockedProtectionLava = BOOL
+	elif upgrade == list.protectionHeat:
+		unlockedProtectionHeat = BOOL
+	elif upgrade == list.protectionCold:
+		unlockedProtectionCold = BOOL
+	elif upgrade == list.protectionWater:
+		unlockedProtectionWater = BOOL
+	elif upgrade == list.protectionAcid:
+		unlockedProtectionAcid = BOOL
+	elif upgrade == list.protectionLava:
+		unlockedProtectionLava = BOOL
+	else:
+		print("Null Upgrade Unlocked " + str(upgrade) + " " + str(BOOL))
