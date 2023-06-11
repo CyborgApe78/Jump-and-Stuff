d5e4c55678434824deeb7a7fac444ecbcbad15c2
Matt <mjcutshaw@gmail.com>
2023-06-11 00:57:16 -0400

    hide on startup

--- src/object/scripts/StartingLocation.gd
+++ src/object/scripts/StartingLocation.gd
@@ -3,6 +3,9 @@ extends Node2D
 
 var CheckpointSystem: Resource = preload("res://src/resources/CheckpointSystem.tres")
 
+func _ready() -> void:
+	visible = false
+
 func  _enter_tree() -> void:
 	CheckpointSystem.startingArea = global_position
 
