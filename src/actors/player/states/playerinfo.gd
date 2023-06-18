f7fed6491bd118bd8da7c47b27e5aedde5fcabed
Matt <mjcutshaw@gmail.com>
2023-06-18 03:07:47 -0400

    lowered dash speed

--- src/actors/player/states/playerinfo.gd
+++ src/actors/player/states/playerinfo.gd
@@ -48,7 +48,7 @@ func update_stats() -> void:
 	frictionGround = moveSpeed / stats.baseFriction
 	accelerationAir = moveSpeed / (stats.baseAcceleration * stats.airModifier)
 	frictionAir = moveSpeed / (stats.baseFriction * stats.airModifier)
-	dashVelocity = moveSpeed * 5
+	dashVelocity = moveSpeed * 3
 	
 	jumpHeight = stats.baseJumpHeight * Util.tileSize
 	gravityJump = 2 * jumpHeight / pow(jumpTimeToPeak, 2)
