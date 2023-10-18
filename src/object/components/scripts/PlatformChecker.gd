0cc282bd1e0c3368e2b31d5436db73094faaba0b
mjcutshaw <mjcutshaw@gmail.com>
2023-10-18 03:17:15 -0400

    created platform area for better checks on actors

---
+++ src/object/components/scripts/PlatformChecker.gd
@@ -0,0 +1,6 @@
+extends Area2D
+class_name PlatformCheckerComponent
+
+## Area for platforms to check instead of looking at the whole collision shape
+
+#TODO: check if on player or enemy and set collision layer
