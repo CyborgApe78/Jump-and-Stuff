4edab22bff4d4568d6677511ef4c3da5c6920398
Matt <mjcutshaw@gmail.com>
2023-10-24 03:03:11 -0400

    created neighbor detector and moving platform connected movement

---
+++ src/object/components/scripts/NeighborDetector.gd
@@ -0,0 +1,27 @@
+extends Area2D
+
+
+@onready var parent: Node = get_parent()
+@onready var parentType: String = parent.get_script().resource_path.get_file()
+
+var neighbors: Array 
+
+
+func _ready() -> void:
+	await get_tree().process_frame
+	get_neighbors()
+	
+
+
+func reset() -> void:
+	pass #LOOKAT: might crash if reset afterwards
+#	neighbors.clear()
+#	await get_tree().process_frame
+#	get_neighbors()
+
+
+func get_neighbors() -> void:
+	for i in get_overlapping_bodies():
+		if i.get_script().resource_path.get_file() == parentType and i != parent:
+#			print(str(parent) + " is touch " + str(i))
+			neighbors.append(i)
--- src/object/prefab/platforms/scripts/platform_falling.gd
+++ src/object/prefab/platforms/scripts/platform_falling.gd
@@ -7,17 +7,21 @@ signal spawned
 
 @onready var startingPosition: Vector2 = global_position
 
+@export_group("Connections")
 @export var labelTime: Label
 @export var timerReset: Timer
 @export var timerFall: Timer
 @export var collision: CollisionShape2D
 @export var areaCounter: Area2D
+@export var neighborDetector: Area2D
 
+@export_group("")
 @export_range(0.05, 10, 0.5) var timeReset: float = 1
 @export_range(0.05, 10, 0.5) var timeFall: float = 3
 @export var fallSpeed: int = 200
+@export var connectedMovement: bool = false
 
-enum state {idle, fall}
+enum state {idle, shake, fall}
 var currentState
 
 
@@ -25,14 +29,19 @@ func _ready() -> void:
 	timerReset.wait_time = timeReset
 	timerFall.wait_time = timeFall
 	currentState = state.idle
+	set_time_label(timerFall.wait_time)
 
 
 func _physics_process(delta: float) -> void:
 	if !timerFall.is_stopped():
-		labelTime.text = str(round(timerFall.time_left))
+		set_time_label(round(timerFall.time_left))
 	match currentState:
 		state.idle:
 			pass
+		state.shake:
+			if connectedMovement:
+				for i in neighborDetector.neighbors:
+					i.set_time_label(round(timerFall.time_left)) 
 		state.fall:
 			position.y += fallSpeed * delta
 
@@ -48,15 +57,22 @@ func reset() -> void:
 
 func landed() -> void:
 	timerFall.start()
+	currentState = state.shake
 
 
 func cleared() -> void:
-	timerFall.stop()
-	labelTime.text = str(timerFall.wait_time)
+	if currentState != state.fall:
+		currentState = state.idle
+		timerFall.stop()
+		set_time_label(timerFall.wait_time)
 
 
 func _on_fall_timeout() -> void:
 	currentState = state.fall
+	
+	if connectedMovement:
+		for i in neighborDetector.neighbors:
+			i.currentState = state.fall
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
@@ -67,3 +83,7 @@ func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 
 func _on_reset_timeout() -> void:
 	reset()
+
+
+func set_time_label(info: float) -> void:
+	labelTime.text = str(info)
