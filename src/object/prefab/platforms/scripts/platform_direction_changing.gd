7d0554b20fa6735e47a73cc6242ab3099a375610
Matt <mjcutshaw@gmail.com>
2023-10-29 23:28:54 -0400

    new platform

---
+++ src/object/prefab/platforms/scripts/platform_direction_changing.gd
@@ -0,0 +1,110 @@
+extends AnimatableBody2D
+
+
+## When enities lands on platform it moves in the direction of the arrow
+#TODO: make a variant that changes direction after landing
+#lookat: should this be using move and collide?
+#FIXME: player can;t jump off up moving platform, look at moving platform on player
+
+signal spawned
+
+enum state {idle, reset, move}
+enum direction {Null, up, right, down, left}
+
+@onready var startingPosition: Vector2 = global_position
+
+@export var arrow: Node2D
+@export var timerReset: Timer
+@export var collision: CollisionShape2D
+@export var areaCounter: Area2D
+
+@export var startingDirection: direction:
+	set(v):
+		startingDirection = v
+		turn_arrow(v)
+
+@export_range(0.05, 10, 0.5) var timeReset: float = 1
+@export var movingSpeed: int = 200 #TODO: function for blocks per sec
+
+var currentState: int
+var currentDirection: int
+
+
+func _ready() -> void:
+	turn_arrow(startingDirection)
+	timerReset.wait_time = timeReset
+	currentState = state.idle
+#	currentDirection = startingDirection
+	
+	if startingDirection == direction.Null:
+		EventBus.error.emit(str("moving direction null: " + str(name) + " at " + str(global_position)))
+
+
+func _physics_process(delta: float) -> void:
+	match currentState:
+		state.idle:
+			pass
+		state.reset:
+			pass
+		state.move:
+			if currentDirection == direction.up:
+				position.y -= movingSpeed * delta
+			elif currentDirection == direction.down:
+				position.y += movingSpeed * delta
+			elif currentDirection == direction.left:
+				position.x -= movingSpeed * delta
+			elif currentDirection == direction.right:
+				position.x += movingSpeed * delta
+
+
+func reset() -> void:
+	currentState = state.idle
+	global_position = startingPosition
+	collision.set_deferred("disabled", false)
+	visible = true
+	spawned.emit()
+	currentDirection = startingDirection
+	turn_arrow(currentDirection)
+
+
+func landed() -> void:
+	
+	print(currentDirection)
+	if currentDirection != 0:
+		if currentDirection == 4:
+			currentDirection = 1
+		else:
+			currentDirection += 1
+	else:
+		currentDirection = startingDirection
+	turn_arrow(currentDirection)
+	
+	if currentState == state.idle:
+		currentState = state.move
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	if currentState == state.move:
+		collision.set_deferred("disabled", true)
+		visible = false
+		timerReset.start()
+		currentState = state.reset
+
+
+func _on_reset_timeout() -> void:
+	reset()
+
+
+func turn_arrow(point) -> void:
+	if point == direction.up:
+		arrow.rotation_degrees = 0
+	elif  point == direction.down:
+		arrow.rotation_degrees = 180
+	elif  point == direction.left:
+		arrow.rotation_degrees = 270
+	elif  point == direction.right:
+		arrow.rotation_degrees = 90
+
+
+func _on_actor_counter_cleared() -> void:
+	pass
