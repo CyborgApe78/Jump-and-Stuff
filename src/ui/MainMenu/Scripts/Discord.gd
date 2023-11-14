d25b2bf9b671153d2414a23ae1db23121ea9a3b4
Matt <mjcutshaw@gmail.com>
2023-11-14 01:48:05 -0500

    removed redundant code

--- src/ui/MainMenu/Scripts/Discord.gd
+++ src/ui/MainMenu/Scripts/Discord.gd
@@ -3,7 +3,3 @@ extends ButtonBase
 
 func _on_pressed():
 	OS.shell_open("https://discord.gg/6SJyu4NtfW")
-
-
-func send_data() -> void:
-	EventBus.cursorPosition.emit(global_position, position, size)
