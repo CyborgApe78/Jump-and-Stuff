da29abffb3a1b66b1b705d44aea00a61a4d0b3c1
Matt <mjcutshaw@gmail.com>
2023-12-09 01:03:12 -0500

    created hidden area

---
+++ src/object/prefab/scripts/HiddenArea.gd
@@ -0,0 +1,28 @@
+extends Polygon2D
+
+## Area becomes visible when player enters
+
+@export_subgroup("Connections")
+@export var collision: CollisionPolygon2D
+
+@export_subgroup("")
+@export var transTime: float = 1.0
+@export var stayVisible: bool = false
+
+
+func _ready() -> void:
+	if polygon.size() > 1:
+		collision.polygon = polygon
+	else:
+		EventBus.error.emit(str("Hidden Area null: " + str(name) + " at " + str(global_position)))
+
+
+func _on_body_entered(body: Node2D) -> void:
+	var tween = create_tween().set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_OUT)
+	tween.tween_property(self, "modulate", Color(0,0,0, 0), transTime).from_current()
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if not stayVisible:
+		var tween = create_tween().set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_OUT)
+		tween.tween_property(self, "modulate", Color(0,0,0, 1), transTime).from_current()
