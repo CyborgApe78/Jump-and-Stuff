09e8fa90d88e95b9e1575fbe87392938a8d18df6
Matt <mjcutshaw@gmail.com>
2024-01-13 01:12:09 -0500

    removed old code

--- src/object/prefab/AbilityBlocks/scripts/abilityBlockBase.gd
+++ src/object/prefab/AbilityBlocks/scripts/abilityBlockBase.gd
@@ -13,11 +13,7 @@ func _ready() -> void:
 
 
 func block_color() -> void:
-	if ability == PlayerAbilities.list.All:
-		modulate = AbilityColor.allColor
-	elif ability == PlayerAbilities.list.JumpAir:
-		modulate = AbilityColor.jumpColor
-	elif ability == PlayerAbilities.list.DashSide:
+	if ability == PlayerAbilities.list.DashSide:
 		modulate = AbilityColor.dashSideColor
 	elif ability == PlayerAbilities.list.DashUp:
 		modulate = AbilityColor.dashUpColor
@@ -25,8 +21,6 @@ func block_color() -> void:
 		modulate = AbilityColor.dashDownColor
 #	elif ability == PlayerAbilities.list.Grapple:
 #		blockColor = AbilityColor.grappleColor
-	elif ability == PlayerAbilities.list.SwimDash:
-		modulate = AbilityColor.swimDashColor
 	elif ability == PlayerAbilities.list.Burrow:
 		modulate = AbilityColor.burrowColor
 #	elif ability == PlayerAbilities.list.Pogo:
