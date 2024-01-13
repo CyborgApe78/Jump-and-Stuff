93c7414e4a85806d8198db4e7ab98ac0f333b10c
Matt <mjcutshaw@gmail.com>
2024-01-13 01:10:03 -0500

    added enemy base class

---
+++ src/actors/enemies/scripts/Enemies.gd
@@ -0,0 +1,10 @@
+extends Actor
+class_name Enemies
+
+@export var rig: Node2D
+
+
+func facing_logic(direction: int): #TODO: add to global
+	if direction != 0 and rig.scale.x != direction:
+		var tween = create_tween().set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_OUT)
+		tween.tween_property(rig, "scale", Vector2(direction, rig.scale.y), 0.4).from_current()
--- src/actors/enemies/scripts/spikesides.gd
+++ src/actors/enemies/scripts/spikesides.gd
@@ -1,4 +1,4 @@
-extends Actor
+extends Enemies
 class_name SpikEsides
 
 ## Slow moving enemy that can be bounced on. Sides hurt player
@@ -42,7 +42,6 @@ enum DIRECTION {LEFT = -1, RIGHT = 1}
 
 @export_group("Connections")
 @export var labelVelocity: Label
-@export var rig: Node2D
 @export var hurtbox: HurtBox
 @export var bouncebox: Area2D #TODO: create class_name
 @export var allyDetector: RayCast2D
@@ -115,10 +114,7 @@ func _physics_process(delta: float) -> void:
 	labelVelocity.text = str(velocity.round())
 
 
-func facing_logic(direction: int): #TODO: add to global
-	if direction != 0 and rig.scale.x != direction:
-		var tween = create_tween().set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_OUT)
-		tween.tween_property(rig, "scale", Vector2(direction, rig.scale.y), 0.4).from_current()
+
 
 
 
