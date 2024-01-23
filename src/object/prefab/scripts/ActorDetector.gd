a032b8eb366cd4fb9c6922052ace4b93b5016006
mjcutshaw <mjcutshaw@gmail.com>
2024-01-22 19:34:36 -0500

    remove layer/mask 1 on load

--- src/object/prefab/scripts/ActorDetector.gd
+++ src/object/prefab/scripts/ActorDetector.gd
@@ -8,10 +8,14 @@ class_name ActorDetector
 signal triggerd
 
 func _ready() -> void:
+	set_collision_mask_value(1, false)
+	set_collision_layer_value(1, false)
+	
 	if playerDetect:
 		set_collision_mask_value(CollisionLayers.Player, true)
 	if enemyDetect:
 		set_collision_mask_value(CollisionLayers.Enemy, true)
+
 
 func _on_body_entered(body: Node2D) -> void:
 	triggerd.emit()
