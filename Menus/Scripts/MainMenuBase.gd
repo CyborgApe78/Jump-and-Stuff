14a31bab7fe708372d1e84330456ae4624af1f3b
mjcutshaw <mjcutshaw@gmail.com>
2024-05-12 20:33:15 -0400

    menu state machine and theme

--- Menus/Scripts/MainMenuBase.gd
+++ Menus/Scripts/MainMenuBase.gd
@@ -4,6 +4,7 @@ class_name MainMenuBase
 
 @export var buttonFirst: Button
 
+
 ## Sets button as focused
 func _ready() -> void:
 	if buttonFirst != null:
--- Menus/Scripts/MainMenuManager.gd
+++ Menus/Scripts/MainMenuManager.gd
@@ -5,6 +5,14 @@ extends CanvasLayer
 @export var mainMenu: MainMenuBase
 @export var settings: MainMenuBase
 
+enum State {
+	Main,
+	Settings
+}
+
+var currentMenu: int
+var previousMenu: int
+
 
 func _ready() -> void:
 	menu_hid()
@@ -13,25 +21,47 @@ func _ready() -> void:
 
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("ui_back"):
-		if settings.visible:
+		## Return to main menu
+		if currentMenu != State.Main:
 			_on_main_menu_pressed()
+		elif currentMenu == State.Main:
+		### Exit game when back/cancel is pressed
+		##TEMP: add popup for confirmation, use ConfirmationDialog node
+			get_tree().quit()
 	else:
 		return
 
 
+## Hides all menus
 func menu_hid() -> void:
 	mainMenu.visible = false 
 	settings.visible = false
 
-
-
+## Show main menu
 func _on_main_menu_pressed() -> void:
 	menu_hid()
-	startingMenu.visible = true
+	mainMenu.visible = true
+	change_menu(State.Main)
 
 
+## Show settings menu
 func _on_settings_pressed() -> void:
 	menu_hid()
 	settings.visible = true
+	change_menu(State.Settings)
 
 
+## What to do when back button is pressed
+func _on_back_pressed() -> void:
+	if previousMenu == State.Main:
+		_on_main_menu_pressed()
+	elif previousMenu == State.Settings:
+		_on_settings_pressed()
+	else:
+		print("Unknown Menu")
+
+
+## keeps track of what menu is active
+func change_menu(menu: int) -> void:
+	previousMenu = currentMenu
+	currentMenu = menu
